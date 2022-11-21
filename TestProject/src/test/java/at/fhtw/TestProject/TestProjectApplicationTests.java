d60dac8dc5fba7eed923d73d91e4f6f7719fe83d
Rajvinder Singh <ic21b088@technikum-wien.at>
2022-11-21 19:56:02 +0100

    First commit

---
+++ TestProject/src/test/java/at/fhtw/TestProject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.TestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
